e7703cb4951c0317d4f4a5240455c31012d14dbf
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-01-07 10:43:56 +0100

    Add files via upload

---
+++ SimpleCalculator/SimpleCalculator/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        double value1;
+        string operation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "1";
+            button2.Text = "2";
+            button3.Text = "3";
+            button4.Text = "4";
+            button5.Text = "5";
+            button6.Text = "6";
+            button7.Text = "7";
+            button8.Text = "8";
+            button9.Text = "9";
+            button10.Text = "0";
+            button11.Text = "+";
+            button12.Text = "-";
+            button13.Text = "x";
+            button14.Text = "/";
+            button15.Text = "Canc";
+            button16.Text = "Calculate";
+            button17.Text = ".";
+
+            label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "4";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "5";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "6";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "7";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "8";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "9";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            value1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            operation = "+";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            value1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            operation = "-";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            value1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            operation = "x";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            value1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            operation = "/";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ".";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            double value2;
+            double result;
+
+            value2 = Convert.ToDouble(textBox1.Text);
+
+            if (operation == "+")
+            {
+                result = (value1 + value2);
+                textBox1.Text = Convert.ToString(result);
+                value1 = result;
+            }
+
+            if (operation == "-")
+            {
+                result = (value1 - value2);
+                textBox1.Text = Convert.ToString(result);
+                value1 = result;
+            }
+
+            if (operation == "x")
+            {
+                result = (value1 * value2);
+                textBox1.Text = Convert.ToString(result);
+                value1 = result;
+            }
+
+            if (operation == "/")
+            {
+                if (value2 == 0)
+                {
+                    textBox1.Text = "Cannot divide by zero";
+
+                }
+                else
+                {
+                    result = (value1 / value2);
+                    textBox1.Text = Convert.ToString(result);
+                    value1 = result;
+                }     
+            }
+        }
+    }
+}
